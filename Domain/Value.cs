1b691d949d0accaaeb792fec6db79e639483b99c
ellymaimon <eliranmaimon@gmail.com>
2019-08-25 19:08:04 -0700

    Initial commit

---
+++ Domain/Value.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        //entity framework is convention based. if prop called Id, then it will become primary key
+        //because its an int, then it will config db to use auto-generated number
+        //this style of DB creation is called code-first
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
+
